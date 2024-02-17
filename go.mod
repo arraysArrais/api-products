5870e2214432686c51635560c9e2fae834d4641d
arraysArrais <joaopedroarrais@gmail.com>
2024-02-17 12:43:25 -0300

    setup entities & category DB methods

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/arraysArrais/api-products
+
+go 1.22.0
+
+require github.com/google/uuid v1.6.0
---
+++ internal/database/category_db.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/arraysArrais/api-products/internal/entity"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{db: db}
+}
+
+func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := cd.db.Query("SELECT id, name FROM categories")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close() //executa após o término da execução dos demais blocos de código
+
+	var categories []*entity.Category
+
+	for rows.Next() {
+		var category entity.Category
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, &category)
+	}
+
+	return categories, nil
+}
+
+func (cd *CategoryDB) GetCategoryById(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := cd.db.QueryRow("SELECT id, name FROM categories where id = ?", id).Scan(&category.ID, &category.Name) //queryRow para retornar um único resultado
+
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (cd *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := cd.db.Exec("INSERT INTO categories (id, name) values(?, ?)", category.ID, category.Name)
+	if err != nil {
+		return "", err
+	}
+
+	return category.ID, nil
+}
