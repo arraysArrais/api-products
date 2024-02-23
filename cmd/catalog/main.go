783e359cdea1dd942bbb418c6b3f3154a7e0cf4a
arraysArrais <joaopedroarrais@gmail.com>
2024-02-22 23:54:18 -0300

    db script

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go-api-products")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-api-products")
 	if err != nil {
 		panic(err.Error())
 	}
